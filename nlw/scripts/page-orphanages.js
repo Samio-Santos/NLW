bac31aaea83a7664f229e725e149f02e788f0ccb
Samio Santos <71763791+Samio-Santos@users.noreply.github.com>
2020-10-15 03:46:25 -0300

    update

--- nlw/scripts/page-orphanages.js
+++ nlw/scripts/page-orphanages.js
@@ -1,8 +1,21 @@
-const map = L.map("mapid").setView([51.505, -0.09], 13);
+const map = L.map("mapid").setView([-23.7044881, -46.8725772], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
+const icon = L.icon({
+  iconUrl: "./images/map-marker.svg",
+  iconsize: [58, 68],
+  iconAnchor: [29, 68],
+  popAnchor: [170, 2],
+});
+
+const popup = L.popup({
+  closeButton: false,
+  classeName: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./images/arrow-white.svg"> </a>'
+);
+
+L.marker([-23.7044881, -46.8725772], { icon }).addTo(map).bindPopup(popup);
